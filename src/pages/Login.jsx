dfbc8ef2ed88996d28ff371a23375d6f56947296
Maria Hussain <162424488+MARIAHUSSAIN123@users.noreply.github.com>
2026-04-10 13:49:40 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,47 @@
 import { useState } from "react";
 import axios from "axios";
-import "../App.css";   // ✅ IMPORTANT FIX
+import "../App.css";
 
-function Login() {
+export default function Login() {
   const [data, setData] = useState({
     email: "",
-    password: ""
+    password: "",
   });
-
-  const API = import.meta.env.VITE_API_URL;
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post(`${API}/api/auth/login`, data);
+      const res = await axios.post(import.meta.env.VITE_API_URL+"/api/auth/login", data);
       alert(res.data.message);
     } catch (err) {
       alert(err.response?.data?.message || "Error");
     }
   };
 
-  return (
-    <div className="container">
-      <div className="form-box">
-        <h2>Login</h2>
+ return (
+  <div className="container">
+    <div className="form-box">
+      <h2>Login</h2>
 
-        <input
-          placeholder="Email"
-          onChange={(e) =>
-            setData({ ...data, email: e.target.value })
-          }
-        />
+      <input
+        placeholder="Email"
+        onChange={(e) => setData({ ...data, email: e.target.value })}
+      />
 
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) =>
-            setData({ ...data, password: e.target.value })
-          }
-        />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setData({ ...data, password: e.target.value })}
+      />
 
-        <button onClick={handleLogin}>Login</button>
-      </div>
+      <button onClick={handleLogin}>Login</button>
+
+      <p>
+        Don't have account?
+        <span onClick={() => window.location.href = "/signup"}>
+          {" "}Signup
+        </span>
+      </p>
     </div>
-  );
+  </div>
+);
 }
-
-export default Login;
